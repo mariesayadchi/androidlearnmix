09620e265541968ef3f02289534b4a5538c3f708
mariesayadchi <marie.sayadchi@gmail.com>
2015-03-12 11:37:22 +0200

    new arrangement

---
+++ learnmix_phone/app/src/main/java/com/example/marie/androidlearnmix/LoggedinPageActivity.java
@@ -0,0 +1,69 @@
+package com.example.marie.androidlearnmix;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+
+public class LoggedinPageActivity extends ActionBarActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Get the message from the intent
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        setContentView(R.layout.activity_loggedin_page);
+
+        // Create the text view
+        TextView textView = (TextView)findViewById(R.id.textView1);
+        textView.setTextSize(30);
+        textView.setText(message);
+
+
+        final TextView tview = (TextView) findViewById(R.id.textView2);
+        tview.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // request your webservice here. Possible use of AsyncTask and ProgressDialog
+                // show the result here - dialog or Toast
+    open_menu("search");           }
+
+        });
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_loggedin_page, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void open_menu(String where){
+
+
+    }
+}
